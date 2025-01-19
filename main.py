cf189bfc40f180d48036feeb9d808a23c11f8b5b
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-19 20:21:35 +0100

    made better way to introduce shortcuts

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-#PROTOTYPE
-# This is the main file for the prototype of the project. It will be used to test the functionality of the project.
 import time
-
 from pynput import keyboard
 
 # Test data
 shortcuts = {
-    "mpro": "correoprofesional@gmail.com",
-    "mpers": "correopersonal@gmail.com",
-    "ulinked": "https://www.linkedin.com/in/usuario",
-    "ugit": "github.com/usuario",
-    "prechazo": "Estimado [nombre] [apellido],\n\nLamentablemente, no podemos continuar con su solicitud de empleo. Agradecemos su interés en nuestra empresa y le deseamos éxito en sus futuros proyectos.\n\nAtentamente,\n[Nombre] [Apellido]\n[Posición]",
+    "mpro": "correoprofesional@gmail.com ",
+    "mpers": "correopersonal@gmail.com ",
+    "ulinked": "https://www.linkedin.com/in/usuario ",
+    "ugit": "github.com/usuario ",
+    "prechazo": "Estimado [nombre] [apellido],\n\nLamentablemente, no podemos continuar con su solicitud de empleo. Agradecemos su interés en nuestra empresa y le deseamos éxito en sus futuros proyectos.\n\nAtentamente,\n[Nombre] [Apellido]\n[Posición] ",
 }
 
 reading = False
 command = ""
-trigger = "@"
 controller = keyboard.Controller()
+confirm_key = keyboard.Key.space
 
 def on_press(key):
     global reading
     global command
-    global automatic_typing
     try:
         # Stops reading commands
         if key == keyboard.Key.esc:
@@ -30,7 +26,8 @@ def on_press(key):
             print("Stop reading")
 
         # User is typing a command
-        elif hasattr(key, 'char') and key.char == trigger and not automatic_typing:
+        elif hasattr(key, 'char') and any(key.char == k[0] for k in shortcuts.keys()):
+            command += key.char
             reading = True
 
         # Deletes the last character of the command
@@ -50,26 +47,27 @@ def on_press(key):
                 reading = False
                 command = ""
 
-            # Command is found
-            elif command in shortcuts:
+        # Command is found
+        if key == confirm_key and reading:
+            if command in shortcuts:
                 print("Command found")
                 print(shortcuts[command])
 
                 # Delete command
-                for i in range(len(command)):
-                    controller.press(keyboard.Key.backspace)
-                    controller.release(keyboard.Key.backspace)
-
-                # Delete command trigger
-                for i in range(len(trigger)):
+                for _ in range(len(command) + 1) :
                     controller.press(keyboard.Key.backspace)
                     controller.release(keyboard.Key.backspace)
 
                 # Type the result
+                time.sleep(0.1)
                 controller.type(shortcuts[command])
 
                 command = ""
                 reading = False
+            else:
+                print("Command not found")
+                reading = False
+                command = ""
 
     except AttributeError:
         pass
