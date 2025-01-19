8213c713c0568eb4caff6052058d75cb22b2161a
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-19 17:24:08 +0100

    program actually types "to the computer"

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 #PROTOTYPE
 # This is the main file for the prototype of the project. It will be used to test the functionality of the project.
+import time
+
 from pynput import keyboard
 
 # Test data
 shortcuts = {
-    "@mpro": "correoprofesional@gmail.com",
-    "@mpers": "correopersonal@gmail.com",
-    "@ulinked": "https://www.linkedin.com/in/usuario",
-    "@ugit": "github.com/usuario",
-    "@prechazo": "Estimado [nombre] [apellido],\n\nLamentablemente, no podemos continuar con su solicitud de empleo. Agradecemos su interés en nuestra empresa y le deseamos éxito en sus futuros proyectos.\n\nAtentamente,\n[Nombre] [Apellido]\n[Posición]",
+    "mpro": "correoprofesional@gmail.com",
+    "mpers": "correopersonal@gmail.com",
+    "ulinked": "https://www.linkedin.com/in/usuario",
+    "ugit": "github.com/usuario",
+    "prechazo": "Estimado [nombre] [apellido],\n\nLamentablemente, no podemos continuar con su solicitud de empleo. Agradecemos su interés en nuestra empresa y le deseamos éxito en sus futuros proyectos.\n\nAtentamente,\n[Nombre] [Apellido]\n[Posición]",
 }
 
 reading = False
 command = ""
+trigger = "º"
+controller = keyboard.Controller()
 
 def on_press(key):
     global reading
@@ -25,9 +29,8 @@ def on_press(key):
             print("Stop reading")
 
         # User is typing a command
-        elif hasattr(key, 'char') and key.char == "@":
+        elif hasattr(key, 'char') and key.char == trigger:
             reading = True
-            command += "@"
 
         # Deletes the last character of the command
         elif key == keyboard.Key.backspace and reading:
@@ -50,8 +53,22 @@ def on_press(key):
             elif command in shortcuts:
                 print("Command found")
                 print(shortcuts[command])
+
+                # Type result
+                for i in range(len(command)):
+                    controller.press(keyboard.Key.backspace)
+                    controller.release(keyboard.Key.backspace)
+
+                #Delete command trigger
+                for i in range(len(trigger)):
+                    controller.press(keyboard.Key.backspace)
+                    controller.release(keyboard.Key.backspace)
+
+                controller.type(shortcuts[command])
+
                 reading = False
                 command = ""
+
     except AttributeError:
         pass
 
