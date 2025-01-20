b18ee62167015acd15b627c3b37279a9644b57b3
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-20 10:34:59 +0100

    created crud file and methods

---
+++ crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+from models import Shortcut
+
+
+def create_shortcut(db: Session, command: str, result: str):
+    db_shortcut = Shortcut(command=command, result=result)
+    db.add(db_shortcut)
+    db.commit()
+    db.refresh(db_shortcut)
+    return db_shortcut
+
+
+def get_shortcut(db: Session, command: str):
+    return db.query(Shortcut).filter(command == Shortcut.command).first()
+
+
+def get_shortcuts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Shortcut).offset(skip).limit(limit).all()
+
+
+def update_shortcut(db: Session, command: str, result: str):
+    db_shortcut = get_shortcut(db, command)
+    if db_shortcut:
+        db_shortcut.command = command
+        db_shortcut.result = result
+        db.commit()
+        db.refresh(db_shortcut)
+    return db_shortcut
+
+
+def delete_shortcut(db: Session, command: str):
+    db_shortcut = get_shortcut(db, command)
+    if db_shortcut:
+        db.delete(db_shortcut)
+        db.commit()
+    return db_shortcut
