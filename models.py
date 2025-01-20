988d669d052615a249aac1cc24b27baaabfc3850
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-20 10:27:38 +0100

    created database file

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, String
+from database import Base
 
-class Shortcut():
+class Shortcut(Base):
     __tablename__ = 'shortcuts'
     command = Column(String, primary_key=True)
     result = Column(String)
